d67c6e53ff6ebc6637c9cfd7b944e058584f63c0
syedibrahim <syed.ibrahim@narayanagroup.com>
2024-02-29 21:43:39 +0530

    minor change

--- lib/presentation/login/page/dummy_screen.dart
+++ lib/presentation/login/page/dummy_screen.dart
@@ -1,5 +1,4 @@
 import 'package:clean_architecture/presentation/login/bloc/login_bloc.dart';
-import 'package:clean_architecture/presentation/login/bloc/login_screen_states.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
